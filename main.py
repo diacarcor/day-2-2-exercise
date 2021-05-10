15f866f0b988bb3584a6ea0564bc9bf23d4062c0
diacarcor <>
2021-05-10 09:29:59 +0000

    Version 2.0

--- main.py
+++ main.py
@@ -4,8 +4,22 @@ weight = input("enter your weight in kg: ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-bmi = float(weight) / float(height) ** 2
-print(int(bmi))
+bmi = weight / height ** 2
+
+if bmi < 18.5:
+  interpretation = "you are underweight."
+elif bmi < 25:
+  interpretation = "you have a normal weight."
+elif bmi < 30:
+  interpretation = "you are slightly overweight."
+elif bmi < 35:
+  interpretation = "you are obese."
+else:
+  interpretation = "you are clinically obese."
+
+message = f"Your BMI is {round(bmi)}, {interpretation}"
+
+print(message)
 
 
 
